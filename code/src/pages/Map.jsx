2fe5b748c5bee2e9b03648017d61d909e23c62b1
IVYEVOL <1453727774@qq.com>
2023-05-10 01:54:42 +0100

    ddddd

--- code/src/pages/Map.jsx
+++ code/src/pages/Map.jsx
@@ -5,6 +5,175 @@ import Geocode from "react-geocode";
 import './map.scss'
 import axios from 'axios';
 import { MarkerClusterer } from '@react-google-maps/api';
+import 'antd/dist/antd.min.css'
+import { TreeSelect } from 'antd';
+
+
+const { SHOW_PARENT } = TreeSelect;
+
+const treeData = [
+    {
+        title: 'Type',
+        value: '0-0',
+        key: '0-0',
+        children: [
+            {
+                title: 'House',
+                value: '0-0-0',
+                key: '0-0-0',
+            },
+            {
+                title: 'Flat / Apartment',
+                value: '0-0-1',
+                key: '0-0-1',
+            },
+            {
+                title: 'New development',
+                value: '0-0-2',
+                key: '0-0-2',
+            },
+            {
+                title: 'Duplex',
+                value: '0-0-3',
+                key: '0-0-3',
+            },
+        ],
+    },
+    {
+        title: 'No. of Bedrooms',
+        value: '0-1',
+        key: '0-1',
+        children: [
+            {
+                title: '1',
+                value: '0-1-1',
+                key: '0-1-1',
+            },
+            {
+                title: '2',
+                value: '0-1-2',
+                key: '0-1-2',
+            },
+            {
+                title: '3',
+                value: '0-1-3',
+                key: '0-1-3',
+            },
+        ],
+    },
+    {
+        title: 'No. of Bathrooms',
+        value: '0-2',
+        key: '0-2',
+        children: [
+            {
+                title: '1',
+                value: '0-2-0',
+                key: '0-2-0',
+            },
+            {
+                title: '2',
+                value: '0-2-1',
+                key: '0-2-1',
+            },
+            {
+                title: '3',
+                value: '0-2-2',
+                key: '0-2-2',
+            },
+        ],
+    },
+    
+    {
+        title: 'No. of Receptions',
+        value: '0-3',
+        key: '0-3',
+        children: [
+            {
+                title: '1',
+                value: '0-3-0',
+                key: '0-3-0',
+            },
+            {
+                title: '2',
+                value: '0-3-1',
+                key: '0-3-1',
+            },
+            {
+                title: '3',
+                value: '0-3-2',
+                key: '0-3-2',
+            },
+        ],
+    },
+    {
+        title: 'City',
+        value: '0-4',
+        key: '0-4',
+        children: [
+            {
+                title: 'London',
+                value: '0-4-0',
+                key: '0-4-0',
+            },
+            {
+                title: 'Lillie Square',
+                value: '0-4-1',
+                key: '0-4-1',
+            },
+            {
+                title: 'Surrey',
+                value: '0-4-2',
+                key: '0-4-2',
+            },
+        ],
+    },
+    {
+        title: 'No. of Bathrooms',
+        value: '0-5',
+        key: '0-5',
+        children: [
+            {
+                title: '1',
+                value: '0-5-0',
+                key: '0-5-0',
+            },
+            {
+                title: '2',
+                value: '0-5-1',
+                key: '0-5-1',
+            },
+            {
+                title: '3',
+                value: '0-5-2',
+                key: '0-5-2',
+            },
+        ],
+    },
+    {
+        title: 'Prices',
+        value: '0-6',
+        key: '0-6',
+        children: [
+            {
+                title: '1-1000',
+                value: '0-6-0',
+                key: '0-6-0',
+            },
+            {
+                title: '2-1000',
+                value: '0-6-1',
+                key: '0-6-1',
+            },
+            {
+                title: '3-1000',
+                value: '0-6-2',
+                key: '0-6-2',
+            },
+        ],
+    },
+];
+
 
 const containerStyle = {
     width: '900px',
@@ -20,10 +189,32 @@ const ServiceMap = () => {
     const londonCenter = { lat: 51.50718801866831, lng: -0.1278400477978338 };
     const [locations, setLocations] = useState([]);
 
+    // --------------------------------筛选------------------------------------------
+    const [value, setValue] = useState(['0-0-0']);
+
+    const onChange = (newValue) => {
+        console.log('onChange ', value);
+        setValue(newValue);
+    };
+
+    const tProps = {
+        treeData,
+        value,
+        onChange,
+        treeCheckable: true,
+        showCheckedStrategy: SHOW_PARENT,
+        placeholder: 'Please select',
+        style: {
+            width: '100%',
+        },
+    };
+
+    // --------------------------------筛选------------------------------------------
+
     useEffect(() => {
         const fetchData = async () => {
             const response = await axios.get('/london.json');
-            const properties = response.data.slice(0, 2000);
+            const properties = response.data.slice(0, 100);
             const postalCodes = properties.map(property => property.Postal_Code);
             // console.log("PostalCodes:", postalCodes);
             const promises = postalCodes.map(postcode =>
@@ -40,49 +231,59 @@ const ServiceMap = () => {
         fetchData();
     }, []);
 
-    const handleClick = (clusterer, locations, index) =>{
+    const handleClick = (clusterer, locations, index) => {
         console.log(locations[index].lat);
     }
+
 
     const renderMap = () => {
         return (
             <div className='map'>
                 <div className='google'><GoogleMap
-                mapContainerStyle={containerStyle}
-                center={londonCenter}
-                zoom={10}
-            >
-                 <MarkerClusterer>
-                 {clusterer =>
-                    locations.map((location, index) => (
-                        <Marker key={index} position={location} clusterer={clusterer} onClick={() => handleClick(clusterer, locations, index)}/>
-                    ))
-                }
-                 </MarkerClusterer>
-            </GoogleMap>
-            </div>
-            <div className='others'>
-                Tools
-            </div>
+                    mapContainerStyle={containerStyle}
+                    center={londonCenter}
+                    zoom={10}
+                >
+                    <MarkerClusterer>
+                        {clusterer =>
+                            locations.map((location, index) => (
+                                <Marker key={index} position={location} clusterer={clusterer} onClick={() => handleClick(clusterer, locations, index)} />
+                            ))
+                        }
+                    </MarkerClusterer>
+                </GoogleMap>
+                </div>
+                <div className='others'>
+                    Tools
+                </div>
             </div>
         );
     }
 
     return (
-        <div className='map'>
-            <LoadScript
-                googleMapsApiKey="AIzaSyCkT6rPwlprpK8qnwG4SMnnloCsp7NcJkk"
-                language="en"
-                libraries={libraries}
-                className='google'
-            >
-                {locations.length > 0 && renderMap()}
-            </LoadScript>
+        <div>
+            <TreeSelect {...tProps} />
+            <div className='map'>
+                <LoadScript
+                    googleMapsApiKey="AIzaSyCkT6rPwlprpK8qnwG4SMnnloCsp7NcJkk"
+                    language="en"
+                    libraries={libraries}
+                    className='google'
+                >
+                    {locations.length > 0 && renderMap()}
+                </LoadScript>
+            </div>
         </div>
+
     )
 }
 
 export default ServiceMap;
+
+
+
+
+
 
 
 
@@ -135,3 +336,5 @@ export default ServiceMap;
 //     </div>
 //   )
 // }
+
+
